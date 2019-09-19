047cb44c4215fc411bfdc96b3371bdb8e0e20458
nanguoxiaohua <xian985188417473@163.com>
2019-09-19 11:16:39 +0800

    创建

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+// import './styles/index.less'
 
+import ElementUI from 'element-ui'
+
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
